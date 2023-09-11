32e8b64762aaf3aad0b108358e4a38b26ef57863
duypk <duypk@unit.com.vn>
2023-09-11 20:17:20 +0700

    [duypk] commit src code

---
+++ Java/QuanLyBanThan/src/main/java/com/vn/dev/QuanLyBanThanApplication.java
@@ -0,0 +1,13 @@
+package com.vn.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyBanThanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyBanThanApplication.class, args);
+	}
+
+}
---
+++ Java/QuanLyBanThan/src/test/java/com/vn/dev/QuanLyBanThanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyBanThanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
